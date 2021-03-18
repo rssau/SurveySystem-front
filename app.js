cba8954a49d4fcdd250484dda3aa254a897b77c9
rssau <sunzibin@foxmail.com>
2021-03-18 23:38:42 +0800

    完成登录界面

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 String.prototype.getValue = function (parm) {
-    var reg = new RegExp("(^|&)" + parm + "=([^&]*)(&|$)");
-    var r = this.substr(this.indexOf("\?") + 1).match(reg);
-    if (r != null) return unescape(r[2]); return null;
-  }
+	var reg = new RegExp("(^|&)" + parm + "=([^&]*)(&|$)");
+	var r = this.substr(this.indexOf("\?") + 1).match(reg);
+	if (r != null) return unescape(r[2]); return null;
+}
+
+function getArg(argname) {
+	var url = window.location.href;
+	var argvalue = url.getValue(argname);
+	return argvalue;
+}
+
+function logout() {
+	window.location.href = "./login.html";
+}
